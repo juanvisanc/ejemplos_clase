f9284caef658ca6c3d722f70e9ea3494f7f2ce09
Juanvi Sánchez <juanvi@localhost.localdomain>
2015-10-24 14:41:34 +0200

    Acabado Tabla

--- media_vector.php
+++ media_vector.php
@@ -6,6 +6,8 @@
   </head>
   <body>
     <?php
+
+      //Media de los valores contenidos en el vector
         $suma=0;
         $v1=[10,20,30,40,50];
         for ($i=0; $i <count($v1) ; $i++) {
@@ -14,13 +16,18 @@
         $media=$suma/count($v1);
         echo "La media del vector es: $media";
 
-        echo "<table>";
+        //Tabla con el contenido del vector asociativo
         $v2=["nombre"=>"Juanvi","apellido"=>"Sánchez"];
-
+        echo "<table>";
+        echo "<tr>";
         foreach ($v2 as $key => $value) {
-          echo "<td>$key</td><td>$value</td>";
-          
+          echo "<td>$key</td>";
         }
+        echo "<tr>";
+        foreach ($v2 as $key => $value) {
+          echo "<td>$value</td>";
+        }
+        echo "</tr>";
         echo "</table>";
      ?>
   </body>
